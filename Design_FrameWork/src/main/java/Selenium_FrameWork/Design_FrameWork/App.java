2da9103a2f073282e595ea0f848d4c9fcda6fd86
Asma <nasrinislam32965@gmail.com>
2024-02-15 19:29:22 -0600

    add initial new commit

---
+++ Design_FrameWork/src/main/java/Selenium_FrameWork/Design_FrameWork/App.java
@@ -0,0 +1,13 @@
+package Selenium_FrameWork.Design_FrameWork;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
